05f4c89d97ab008e21c7702e3ee30bffbcf82a20
dipak1950 <dipaknimavat1950@gmail.com>
2022-11-18 15:46:51 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -25,6 +25,8 @@ function App() {
 
         I am a professional Front End Developer more than 1️⃣ years of experience in the IT industry. <br></br>I have also worked in front-end as well as bootstrap, wordpress. I am also very adequate in website-related like <br></br> E-commerce,construction,Food-site,Gym,Travelling so, I can integrate any acquirer in min time.<br></br> I have worked in the web also so, I can alternate any flow or behavior also as client requirement. <br></br>I have great experience in creating any new module for any business flow, also for <br></br>customization of existing modules and migrating any module to different versions.<br></br>I believe in hard work and honesty. I am always interested in making long term professional <br></br>relationships with my clients to ensure that every project becomes successful.<br></br> So, if you hire me, I can assure you that you will not regret your decision.<br></br>If you have any questions, then feel free to contact me.<br></br>I'm always happy to answer your questions.</p>
       <h4>Thank You</h4>
+      <p>What is Lorem Ipsum?
+        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
     </div>
   );
 }
