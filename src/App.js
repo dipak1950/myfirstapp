429cb11626b5daff3219ec3eb130e0a4d88b5d72
dipak1950 <dipaknimavat1950@gmail.com>
2022-11-18 15:29:13 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      {<header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,7 +17,14 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header>}
+      <h1>Dipak Nimavat</h1>
+      <h3>Front End Developer</h3>
+      <p>Hello,
+        I am Dipak Nimavat.
+
+        I am a professional Front End Developer more than 1️⃣ years of experience in the IT industry. <br></br>I have also worked in front-end as well as bootstrap, wordpress. I am also very adequate in website-related like <br></br> E-commerce,construction,Food-site,Gym,Travelling so, I can integrate any acquirer in min time.<br></br> I have worked in the web also so, I can alternate any flow or behavior also as client requirement. <br></br>I have great experience in creating any new module for any business flow, also for <br></br>customization of existing modules and migrating any module to different versions.<br></br>I believe in hard work and honesty. I am always interested in making long term professional <br></br>relationships with my clients to ensure that every project becomes successful.<br></br> So, if you hire me, I can assure you that you will not regret your decision.<br></br>If you have any questions, then feel free to contact me.<br></br>I'm always happy to answer your questions.</p>
+      <h4>Thank You</h4>
     </div>
   );
 }
