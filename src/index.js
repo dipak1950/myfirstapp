960006dd8bae8d072a23c8962c90cd8e62b732eb
dipak1950 <dipaknimavat1950@gmail.com>
2022-11-30 16:15:32 +0530

    header-boostrap

--- src/index.js
+++ src/index.js
@@ -1,17 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from '../src/components/App';
+import Header from './components/Header';
+// import App from '../src/components/App';
 // import Gallery from './components/Gallery';
 // import Title from './components/Title';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+  <>
+    <Header logo="images/logo.png" dropdown="Shop" />
+    <Header logo="images/logo1.png" dropdown="About" />
+  </>
+
+
   // <>
   //   <Title/>
   //   <Gallery/>
+  //    <App />
   // </>
-  <App />
 
 
 );
